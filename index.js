a0c31392f7660cffff6215e44a349334f3355c2f
EduEstula1003 <edu.estula@ext.blaze.me>
2022-10-25 23:37:48 -0500

    368. Configurando Express

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Hola Mundo 2");
+const express = require("express");
+
+
+// Crear servidor de Express
+const app = express();
+
+
+// Rutas
+app.get("/", (req, res) => {
+
+    res.json({
+        ok: true
+    });
+})
+
+
+// Escuchar eventos
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en el puerto ${4000}`);
+})
+
