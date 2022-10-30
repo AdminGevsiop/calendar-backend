31d77a4d9b2e612632f5e41ba4580ff112470a3f
EduEstula1003 <edu.estula@ext.blaze.me>
2022-10-29 21:32:09 -0500

    369. Variables de entorno y carpeta publica

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 const express = require("express");
+require("dotenv").config();
 
 
 // Crear servidor de Express
 const app = express();
 
+// Directorio Publico
+app.use( express.static("public") )
+
 
 // Rutas
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
 
-    res.json({
-        ok: true
-    });
-})
+//     res.json({
+//         ok: true
+//     });
+// })
 
 
 // Escuchar eventos
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${4000}`);
 })
 
