0e2b28724d7da591391810e01f0f81fe70d8bb44
Jheysson Urbano <57603867+Jheysson@users.noreply.github.com>
2022-10-30 21:45:00 -0500

    Video hasta el 378

---
+++ controllers/auth.js
@@ -0,0 +1,55 @@
+const { response } = require('express')
+const Usuario = require('../models/Usuario')
+
+const crearUsuario = async (req, res = response) => {
+
+  const { name, email, password } = req.body
+  
+  try{
+    
+    let usuario = await Usuario.findOne({email})
+    console.log('usuario::', usuario)
+  
+    if(usuario){
+      return res.status(400).json({
+        ok: false,
+        msg: 'Ya existe un usuario con este email'
+      })
+    }
+
+    usuario = new Usuario(req.body);
+    await usuario.save()
+
+    res.status(201).json({
+      ok: true,
+      uid: usuario.id,
+      name: usuario.name
+    })
+  }catch(error){
+    res.status(500).json({
+      ok: false,
+      msg: 'Por favor hable con el administrador'
+    })
+  }
+  
+}
+
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body  
+  
+  res.json({
+    ok: true,
+    msg: 'Login',
+    email,
+    password
+  })
+}
+
+const revalidarToken = (req, res = response) => {  
+  res.json({
+    ok: true,
+    msg: 'Renew'
+  })
+}
+
+module.exports = { crearUsuario, loginUsuario, revalidarToken }
