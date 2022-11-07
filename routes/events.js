bb77ecf1612e11dc525b050bee3ce0617c0e80d3
EduEstula1003 <edu.estula@ext.blaze.me>
2022-11-06 21:22:07 -0500

    390. Validar campos necesarios

--- routes/events.js
+++ routes/events.js
@@ -3,14 +3,22 @@
     host + /api/events
 */
 const { Router } = require("express");
+const { check } = require("express-validator");
 const { getEvents, createEvent, updateEvent, deleteEvent } = require("../controllers/events");
+const { isDate } = require("../helpers/isDate");
+const { validateFields } = require("../middlewares/validateFields");
 const { validateJWT } = require("../middlewares/validateJWT");
 
 
 const router = Router();
 router.use(validateJWT)
 
-router.post("/create", [], createEvent);
+router.post("/create", [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+    validateFields
+], createEvent);
 
 router.get("/", [], getEvents);
 
