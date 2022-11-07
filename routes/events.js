9fa12f3a0d437b3fc18131f74f1764928cdd0b29
Kevin Valencia <kevinvalenciadiaz18@gmail.com>
2022-11-06 21:14:59 -0500

    Seción 24

---
+++ routes/events.js
@@ -0,0 +1,40 @@
+/*
+    Event Routes
+    /api/events
+ */
+
+const {Router} = require('express');
+const {check} = require('express-validator');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
+const router = Router();
+router.use(validarJWT);
+//Todas tienen que pasar por la validación de JWT
+//obtener eventos
+router.get( '/', getEventos);
+
+//crear nuevo evento
+router.post( 
+    '/', 
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatorio').custom(isDate),
+        check('end', 'Fecha de fin es obligatorio').custom(isDate),
+        validarCampos
+    ],
+     crearEvento);
+
+//Actualizar evento
+router.put( '/:id', [
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatorio').custom(isDate),
+    check('end', 'Fecha de fin es obligatorio').custom(isDate),
+    validarCampos
+], actualizarEvento);
+
+//Eliminar evento
+router.delete( '/:id', eliminarEvento);
+
+module.exports = router;
