9a4e07404df3c4f11ab46e30393c1e87515e75f7
Jheysson Urbano <57603867+Jheysson@users.noreply.github.com>
2022-11-19 20:49:36 -0500

    3.92

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,11 @@
   /api/events
 */
 const { Router } = require('express')
+const { check } = require('express-validator')
+
+const { validarCampos } = require('../middlewares/validar-campos')
+const { isDate } = require('../helpers/isDate')
+
 const { validarJWT } = require('../middlewares/validar-jwt')
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento }  = require('../controllers/events')
 
@@ -12,7 +17,14 @@ router.use(validarJWT)
 
 router.get('/', getEventos)
 
-router.post('/', crearEvento)
+router.post('/', 
+[
+  check('title', 'El titulo es obligatorio').not().isEmpty(),
+  check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+  check('end', 'Fecha de finalización es obligatoria').custom(isDate),
+  validarCampos
+],
+crearEvento)
 
 router.put('/:id', actualizarEvento)
 
